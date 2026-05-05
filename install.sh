82f302e4d31720f99ef7f7c8e4f4ae11a5f8c3ac
Axxel-otl <Axxel.Inkordia@proton.me>
2026-05-04 18:57:40 -0600

    Fix systemd service installation path in install.sh
    Updated installation script to install systemd service in the correct user directory.

--- install.sh
+++ install.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
 
 PREFIX="${PREFIX:-$HOME/.local}"
+SYSTEMD_USER_DIR="$HOME/.config/systemd/user"
 
+echo "Installing Police..."
+
+# Binarios
 install -Dm755 police "$PREFIX/bin/police"
 install -Dm755 police.sh "$PREFIX/lib/police/police.sh"
-install -Dm644 systemd/police@.service "$PREFIX/lib/systemd/user/police@.service"
+
+# Systemd (ruta correcta)
+mkdir -p "$SYSTEMD_USER_DIR"
+install -Dm644 systemd/police@.service "$SYSTEMD_USER_DIR/police@.service"
+
+# Recargar systemd
+systemctl --user daemon-reload
 
 echo "Installed in $PREFIX"
+echo "Systemd service installed in $SYSTEMD_USER_DIR"
